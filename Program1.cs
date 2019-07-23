3e1a0d7e3c327bbd44295ee5e52e2f9bcfe9253b
GrevictRodriguez <53051776+GrevictRodriguez@users.noreply.github.com>
2019-07-22 22:37:22 -0500

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese T y a°");
+            double T = double.Parse(Console.ReadLine());
+            double aGrados = double.Parse(Console.ReadLine());
+
+
+            double a = aGrados * (Math.PI / 180.0);
+
+            double c = a-(Math.PI/2);
+            
+
+           double z = T * Math.Sin(c);
+           double y = T * Math.Cos(c);
+             
+            Console.WriteLine("El lado z es:" + z);
+           Console.WriteLine("El lado y es:" + y);
+           Console.WriteLine("el angulo c es: " + c);
+        }
+
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese z y c°");
+            double z = double.Parse(Console.ReadLine());
+            double cGrados = double.Parse(Console.ReadLine());
+
+
+            double c = cGrados * (Math.PI / 180.0);
+
+            double a = Math.PI / 2 - c;
+            double aGrados = a * 180 / Math.PI;
+
+            double t = z/ Math.Sin(c);
+            double y = z /Math.Tan(c);
+           
+
+
+            Console.WriteLine("la hipotenusa es:" + t);
+            Console.WriteLine("El lado y es:" + y);
+            Console.WriteLine("el angulo a es: " + aGrados);
+        }
+    }
+}
