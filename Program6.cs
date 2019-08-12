a2b8138c01188f8a53c4aab45dc3f615f786ca09
GrevictRodriguez <53051776+GrevictRodriguez@users.noreply.github.com>
2019-08-12 15:48:57 -0500

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random aleatorio = new Random();
+            int dado1 = 0, dado2 = 0, total = 0;
+            string continuar = "s";
+
+            while (continuar == "s")
+            {
+                dado1 = aleatorio.Next(1, 11);
+                dado2 = aleatorio.Next(1,11);
+                int suma = dado1 + dado2;
+                Console.WriteLine("Carta 1= " + dado1);
+                Console.WriteLine("Carta 2= " + dado2);                
+                
+                if (total < 21)
+                {
+                    total += suma;
+                    Console.WriteLine("Total =" + total);
+                    Console.WriteLine("Desea continuar (s/n): ");
+                    continuar = Console.ReadLine();
+                   
+                }
+                else
+                {
+                    total = 0;
+                    Console.WriteLine("(Eliminado)");
+                    continuar = "n";
+                }
+               
+            }
+            Console.WriteLine("Su total fue " + total + " puntos ");
+            Console.WriteLine("Gracias por jugar");
+
+        }
+
+    }
+}
