e8bfc8bc0a69421db41a624845a0b4736ac00893
GrevictRodriguez <53051776+GrevictRodriguez@users.noreply.github.com>
2019-08-12 20:37:43 -0500

    Add files via upload

--- Program6.cs
+++ Program6.cs
@@ -8,40 +8,53 @@ namespace Tarea6
 {
     class Program
     {
+
+
         static void Main()
         {
             Random aleatorio = new Random();
             int dado1 = 0, dado2 = 0, total = 0;
             string continuar = "s";
 
-            while (continuar == "s")
+
+
+            dado1 = aleatorio.Next(1, 11);
+            dado2 = aleatorio.Next(1, 11);
+            int suma = dado1 + dado2;
+            Console.WriteLine("Carta 1= " + dado1);
+            Console.WriteLine("Carta 2= " + dado2);
+
+            total = suma;
+            Console.WriteLine("Total =" + total);
+            Console.WriteLine("Desea continuar (s/n): ");
+            continuar = Console.ReadLine();
+
+
+
+            while (total <= 21 && continuar == "s")
             {
                 dado1 = aleatorio.Next(1, 11);
-                dado2 = aleatorio.Next(1,11);
-                int suma = dado1 + dado2;
                 Console.WriteLine("Carta 1= " + dado1);
-                Console.WriteLine("Carta 2= " + dado2);                
-                
-                if (total < 21)
+                total += dado1;
+
+                if (total <= 21)
                 {
-                    total += suma;
                     Console.WriteLine("Total =" + total);
                     Console.WriteLine("Desea continuar (s/n): ");
                     continuar = Console.ReadLine();
-                   
+
                 }
                 else
                 {
-                    total = 0;
-                    Console.WriteLine("(Eliminado)");
-                    continuar = "n";
+                    Console.WriteLine("(Perdio)");
+                    Console.WriteLine("Total =" + total);
                 }
-               
             }
             Console.WriteLine("Su total fue " + total + " puntos ");
             Console.WriteLine("Gracias por jugar");
 
-        }
 
+        }
     }
 }
+
